89228c1bd834005544710bf66fbf1fbbaa2e7411
Dave Rowe <info@librarieshacked.org>
2020-11-23 13:59:04 -0800

    Add app code for setup

---
+++ Shared/BibliofileApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibliofileApp.swift
+//  Shared
+//
+//  Created by user187440 on 11/23/20.
+//
+
+import SwiftUI
+
+@main
+struct BibliofileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
